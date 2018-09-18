1ce8f55e1d0513793ce5a7a94dc2bff54cb3242d
liuzz <zz.secrething@gmail.com>
2018-09-18 17:21:11 +0800

    test RandomAccessFile

---
+++ learn-test/src/main/java/com/secrething/learn/test/RandomRWFileTest.java
@@ -0,0 +1,25 @@
+package com.secrething.learn.test;
+
+import com.secrething.common.util.Assert;
+import com.secrething.common.util.console;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.RandomAccessFile;
+
+/**
+ * Created by liuzz on 2018/9/18 11:26 AM.
+ */
+public class RandomRWFileTest {
+
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("/Users/liuzz58/Desktop/work/jiequ.log");
+        Assert.isTrue(file.exists());
+        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+        console.log(randomAccessFile.readLine());
+        FileDescriptor descriptor = randomAccessFile.getFD();
+        console.log(descriptor);
+
+    }
+}
