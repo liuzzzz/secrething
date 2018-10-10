1a29d9833fc58d1c0b83276a7ca1992c88300610
liuzz <zz.secrething@gmail.com>
2018-10-10 18:11:15 +0800

    生成条码 组合图片

--- learn-test/src/main/java/com/secrething/learn/test/BarcodeUtil.java
+++ learn-test/src/main/java/com/secrething/learn/test/BarcodeUtil.java
@@ -1,12 +1,16 @@
 package com.secrething.learn.test;
 
+import com.secrething.common.util.MesgFormatter;
 import org.apache.commons.lang3.StringUtils;
 import org.krysalis.barcode4j.impl.code39.Code39Bean;
 import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
 import org.krysalis.barcode4j.tools.UnitConv;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by liuzz on 2018/10/10 2:46 PM.
@@ -82,9 +86,104 @@ public class BarcodeUtil {
         }
     }
 
+    /**
+     * @param files 要拼接的文件列表
+     * @param type  1  横向拼接， 2 纵向拼接
+     * @Description:图片拼接 （注意：必须两张图片长宽一致哦）
+     * @author:liuyc
+     * @time:2016年5月27日 下午5:52:24
+     */
+    public static void mergeImage(String[] files, int type, String targetFile) {
+        int len = files.length;
+        if (len < 1) {
+            throw new RuntimeException("图片数量小于1");
+        }
+        File[] src = new File[len];
+        BufferedImage[] images = new BufferedImage[len];
+        int[][] ImageArrays = new int[len][];
+        for (int i = 0; i < len; i++) {
+            try {
+                src[i] = new File(files[i]);
+                images[i] = ImageIO.read(src[i]);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            int width = images[i].getWidth();
+            int height = images[i].getHeight();
+            ImageArrays[i] = new int[width * height];
+            ImageArrays[i] = images[i].getRGB(0, 0, width, height, ImageArrays[i], 0, width);
+        }
+        int newHeight = 0;
+        int newWidth = 0;
+        for (int i = 0; i < images.length; i++) {
+            // 横向
+            if (type == 1) {
+                newHeight = newHeight > images[i].getHeight() ? newHeight : images[i].getHeight();
+                newWidth += images[i].getWidth();
+            } else if (type == 2) {// 纵向
+                newWidth = newWidth > images[i].getWidth() ? newWidth : images[i].getWidth();
+                newHeight += images[i].getHeight() + 20;
+            }
+        }
+        if (type == 1 && newWidth < 1) {
+            return;
+        }
+        if (type == 2 && newHeight < 1) {
+            return;
+        }
+
+        // 生成新图片
+        try {
+            BufferedImage ImageNew = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+            int height_i = 0;
+            int width_i = 0;
+            for (int i = 0; i < images.length; i++) {
+                if (type == 1) {
+                    ImageNew.setRGB(width_i, 0, images[i].getWidth(), newHeight, ImageArrays[i], 0,
+                            images[i].getWidth());
+                    width_i += images[i].getWidth();
+                } else if (type == 2) {
+                    ImageNew.setRGB(0, height_i, newWidth, images[i].getHeight(), ImageArrays[i], 0, newWidth);
+                    height_i += images[i].getHeight() + 20;
+                }
+            }
+            //输出想要的图片
+            ImageIO.write(ImageNew, targetFile.split("\\.")[1], new File(targetFile));
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) {
-        String msg = "DAJLSJDK123213";
-        String path = "/Users/liuzz58/Desktop/barcode.png";
-        generateFile(msg, path);
+        long l = 3999760580591L;
+
+        List<String[]> list = new ArrayList<>();
+        String[] sarr = new String[50];
+        list.add(sarr);
+        int idx = 0;
+        for (int i = 1; i < 10001; i++) {
+            long rad = (long) (Math.random() * 100000);
+            String path = "/Users/liuzz58/Desktop/barcode/barcode{}.png";
+            String fileName = MesgFormatter.format(path, i);
+            long ms = l + rad;
+            generateFile(String.valueOf(ms), fileName);
+            if (idx > 49) {
+                idx = 0;
+                sarr = new String[50];
+                list.add(sarr);
+            }
+            sarr[idx] = fileName;
+            idx++;
+            MesgFormatter.println("create {} barcode",i);
+        }
+        int j = 1;
+        for (String[] arr : list) {
+            mergeImage(arr, 2, "/Users/liuzz58/Desktop/barcode_group/barcode" + j + ".png");
+            MesgFormatter.println("merge {} barcode",j);
+            j++;
+        }
+        //mergeImage(list.toArray(new String[]{}),2,"/Users/liuzz58/Desktop/barcode/barcode1_10.png");
+
     }
 }
