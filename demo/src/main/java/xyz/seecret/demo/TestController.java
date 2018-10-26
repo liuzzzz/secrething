43e4d444de1746c51340214c88e9b6b1bf597224
liuzz <zz.secrething@gmail.com>
2018-10-26 13:31:49 +0800

    docker file

--- demo/src/main/java/xyz/seecret/demo/TestController.java
+++ demo/src/main/java/xyz/seecret/demo/TestController.java
@@ -1,5 +1,6 @@
 package xyz.seecret.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("test")
+@Slf4j
 public class TestController {
     public TestController(){
         //new Throwable().printStackTrace();
     }
     @RequestMapping("/{param1}")
     public String test(@PathVariable("param1") String param1){
-        return param1;
+        log.info("request: - {}",param1);
+        return "result: - "+param1;
 
     }
     /*@RequestMapping("/{param2}")
