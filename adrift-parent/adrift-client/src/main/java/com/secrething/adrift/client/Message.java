54122b694bba777e1ad428bb2c12583280254fad
liuzz <zz.secrething@gmail.com>
2018-11-22 17:34:05 +0800

    聊天,先这样吧

---
+++ adrift-parent/adrift-client/src/main/java/com/secrething/adrift/client/Message.java
@@ -0,0 +1,16 @@
+package com.secrething.adrift.client;
+
+import lombok.Data;
+
+/**
+ * Created by liuzz on 2018/11/22 下午4:36.
+ */
+@Data
+public class Message {
+
+    private String name;
+
+    private String content;
+
+
+}
