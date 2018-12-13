8a63606016005734af79fe4ca104b91c9681bbc0
liuzz <zz.secrething@gmail.com>
2018-12-13 09:35:11 +0800

    gc info

--- common/src/main/java/com/secrething/common/util/MesgFormatter.java
+++ common/src/main/java/com/secrething/common/util/MesgFormatter.java
@@ -1,6 +1,22 @@
 package com.secrething.common.util;
 
+import com.alibaba.fastjson.JSON;
+import com.sun.management.GcInfo;
 import org.apache.commons.lang3.StringUtils;
+
+import javax.management.Notification;
+import javax.management.NotificationBroadcaster;
+import javax.management.NotificationFilter;
+import javax.management.NotificationListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.lang.reflect.Method;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.List;
 
 /**
  * @author liuzz
@@ -58,4 +74,80 @@ public abstract class MesgFormatter {
     public static final void print(String pattern, Object... params) {
         System.out.print(format(pattern, params));
     }
+    private static class GCListener implements NotificationListener {
+
+        @Override
+        public void handleNotification(Notification notification, Object handback) {
+            System.out.println(JSON.toJSONString(notification));
+            final Object gcbean = handback;
+            GcInfo gcInfo = (GcInfo) AccessController.doPrivileged((PrivilegedAction<Object>) () -> {
+                try {
+                    Method method = gcbean.getClass().getMethod("getLastGcInfo");
+                    method.setAccessible(true);
+                    return method.invoke(gcbean);
+                } catch (Exception e) {
+                    throw new IllegalStateException(e);
+                }
+            });
+            System.out.println(JSON.toJSONString(gcInfo));
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        String pname = ManagementFactory.getRuntimeMXBean().getName();
+        List<GarbageCollectorMXBean> list = ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean gcbean: list){
+            NotificationBroadcaster broadcaster = (NotificationBroadcaster) gcbean;
+            broadcaster.addNotificationListener(new GCListener(),null,gcbean);
+            /*AccessController.doPrivileged((PrivilegedAction<Object>) () -> {
+                try {
+                    Method method = gcbean.getClass().getMethod("addNotificationListener",NotificationListener.class, NotificationFilter.class,Object.class);
+                    method.setAccessible(true);
+                    return method.invoke(gcbean,new GCListener(),null,null);
+                } catch (Exception e) {
+                    throw new IllegalStateException(e);
+                }
+            });*/
+        }
+        System.out.println(System.currentTimeMillis());
+        System.gc();
+        System.out.println(System.currentTimeMillis());
+        Thread.sleep(1000);
+        Thread t =  new Thread(() -> {
+            try {
+                Process process = Runtime.getRuntime().exec(format("jstat -gcutil {} 1000 1000",pname.split("@")[0]));
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                for (String line = reader.readLine();null != line;line = reader.readLine()){
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+
+            }
+        });
+        t.setDaemon(true);
+        t.start();
+        Thread.sleep(100000);
+
+    }
+    /*private static Object invoke(final Object target, final String methodName, final Class<?>... args) {
+        return AccessController.doPrivileged(new PrivilegedAction<Object>() {
+            public Object run() {
+                try {
+                    Method method = method(target, methodName, args);
+                    method.setAccessible(true);
+                    return method.invoke(target);
+                } catch (Exception e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        });
+    }
+    private static Method method(Object target, String methodName, Class<?>[] args)
+            throws NoSuchMethodException {
+        try {
+            return target.getClass().getMethod(methodName, args);
+        } catch (NoSuchMethodException e) {
+            return target.getClass().getDeclaredMethod(methodName, args);
+        }
+    }*/
 }
